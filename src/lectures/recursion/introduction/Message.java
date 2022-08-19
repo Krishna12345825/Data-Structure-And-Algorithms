b933e10af79b921a205ad999d8c9a42f60b74858
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-08-19 19:58:05 +0530

    introduction to recursion code

---
+++ src/lectures/recursion/introduction/Message.java
@@ -0,0 +1,32 @@
+package lectures.recursion.introduction;
+
+public class Message {
+    public static void main(String[] args) {
+        // write a function that prints  hello worlds
+        message();
+    }
+
+    static void message() {
+        System.out.println("Hello World");
+        message1();
+    }
+
+    private static void message1() {
+        System.out.println("Hello world");
+        message2();
+    }
+
+    private static void message2() {
+        System.out.println("Hello world");
+        message3();
+    }
+
+    private static void message3() {
+        System.out.println("Hello world");
+        message4();
+    }
+
+    private static void message4() {
+        System.out.println("Hello world");
+    }
+}
