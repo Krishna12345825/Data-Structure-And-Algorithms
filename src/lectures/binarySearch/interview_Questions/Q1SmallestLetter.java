f9552b445b9ab1f3e60eebbb074ff2b95a7388f2
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-08-19 19:58:05 +0530

    Conditional loops code

---
+++ src/lectures/binarySearch/interview_Questions/Q1SmallestLetter.java
@@ -0,0 +1,31 @@
+package lectures.binarySearch.interview_Questions;
+
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+public class Q1SmallestLetter {
+
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'a';
+        char ans = nextGreatestLetter(letters, target);
+        System.out.println(ans);
+    }
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < letters[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
---
+++ src/lectures/conditional_LoopsAnd_Switch/code/loop/Reverse.java
@@ -0,0 +1,18 @@
+package lectures.conditional_LoopsAnd_Switch.code.loop;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int num = 123456;
+
+        int ans = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+
+            ans = ans * 10 + rem;
+        }
+
+        System.out.println(ans);
+    }
+}
