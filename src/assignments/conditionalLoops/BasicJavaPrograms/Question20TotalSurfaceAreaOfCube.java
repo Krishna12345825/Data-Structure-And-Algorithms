68b97356459172ae26ad538731b3487b3fc6a371
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-08-19 19:58:05 +0530

    Conditional loops code

---
+++ src/assignments/conditionalLoops/BasicJavaPrograms/Question20TotalSurfaceAreaOfCube.java
@@ -0,0 +1,14 @@
+package assignments.conditionalLoops.BasicJavaPrograms;
+
+import java.util.Scanner;
+
+// 20. Total Surface Area Of Cube
+public class Question20TotalSurfaceAreaOfCube {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the side of cube ");
+        double side = scanner.nextDouble();
+        double area = 6 * side * side;
+        System.out.println(" Total Surface Area Of Cube " + area);
+    }
+}
