4937ea9761cb9eb11fe45fb224d2f5c02f2a1b7d
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-08-19 19:58:05 +0530

    String lecture code

---
+++ src/lectures/string/Output.java
@@ -0,0 +1,16 @@
+package lectures.string;
+
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+        Integer num = 56;
+        System.out.println(num.toString());
+        System.out.println(num);
+        System.out.println("Krishna");
+        System.out.println(Arrays.toString(new int[]{2, 3, 4, 5}));
+        String name = null;
+        System.out.println(name);
+    }
+}
---
+++ src/lectures/string/PrettyPrinting.java
@@ -0,0 +1,10 @@
+package lectures.string;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.12374f;
+        System.out.printf("Pie: %.3f", Math.PI);
+        System.out.printf("Hello my name is %s and I am %s", "Krishna", "Cool");
+
+    }
+}
