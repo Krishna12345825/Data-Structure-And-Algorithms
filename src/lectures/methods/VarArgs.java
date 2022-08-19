3774a3e133b9defbbcd5fe6190d85363a6962954
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-08-19 19:58:05 +0530

    methods lecture code

---
+++ src/lectures/methods/VarArgs.java
@@ -0,0 +1,32 @@
+package lectures.methods;
+
+import java.util.Arrays;
+
+import static lectures.methods.Overloading.fun;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun();
+        multiple(2, 4, "krishna", "kumar", "Yad-av");
+        demo(12);
+        demo("krishna");
+    }
+
+    static void demo(int... v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+
+    static void demo(String... v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String... v) {
+
+    }
+
+
+    private static void fun(int... v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
