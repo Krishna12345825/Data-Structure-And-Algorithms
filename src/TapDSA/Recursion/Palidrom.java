edfb11270151b9d2b80a4c58c6502d5a772f461b
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-09-26 16:50:19 +0530

    sum of all digits recursiion

---
+++ src/TapDSA/Recursion/Palidrom.java
@@ -0,0 +1,18 @@
+package TapDSA.Recursion;
+
+public class Palidrom {
+    public static void main(String[] args) {
+        String s= "KAKA";
+        System.out.println(palidrom(s,0,s.length()-1));
+
+    }
+    public  static boolean palidrom(String s, int i, int j){
+        if(s.charAt(i)!=s.charAt(j)){
+            return false;
+        }
+        if (i<=j){
+            return true;
+        }
+        return palidrom(s,i+1, j-1);
+    }
+}
