8034dc1d8e4ef2942d856df34024bfd1f0f5bed4
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-03 16:44:52 +0530

    String print subString length 3

---
+++ src/TapDSA/Strings/PrintSubStringLength3.java
@@ -0,0 +1,20 @@
+package TapDSA.Strings;
+// Print substring of length 3
+public class PrintSubStringLength3 {
+    public static void main(String[] args) {
+        String s = "Krishna";
+        int k = 3;
+        subStringLength(s,k);
+    }
+
+    public static void subStringLength(String st, int k){
+        int n = st.length();
+        for (int s = 0; s <= (n - k); s++) {
+            for (int e = s; e <= (s + k) - 1; e++) {
+                System.out.print(st.charAt(e)+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
