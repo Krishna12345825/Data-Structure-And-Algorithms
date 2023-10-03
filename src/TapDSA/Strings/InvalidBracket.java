45c8c93548eccd109e409d9e0725391e499cbbcd
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-03 17:00:09 +0530

    Print Longest non repeating substring

---
+++ src/TapDSA/Strings/InvalidBracket.java
@@ -0,0 +1,24 @@
+package TapDSA.Strings;
+import java.util.Scanner;
+public class InvalidBracket {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int bracket=0, count=0;
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                bracket++;
+            }
+            else {
+                if(bracket <= 0) {
+                    count++;
+                }else {
+                    bracket--;
+                }
+            }
+        }
+        System.out.println(count + bracket);
+    }
+
+}
