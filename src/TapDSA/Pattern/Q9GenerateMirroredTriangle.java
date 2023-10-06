53655f235ee43142ff56fbeec196675a76780ae7
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-06 23:47:03 +0530

    Pattern 9

---
+++ src/TapDSA/Pattern/Q9GenerateMirroredTriangle.java
@@ -0,0 +1,24 @@
+package TapDSA.Pattern;
+
+import java.util.Scanner;
+
+public class Q9GenerateMirroredTriangle {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the Number");
+//        int n = scanner.nextInt();
+        int n=5;
+        pattern9(n);
+    }
+    public static void pattern9(int n){
+        for (int i = 1; i <=n; i++) {
+            for (int k = 1; k<=n-i; k++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <=i; j++) {
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
