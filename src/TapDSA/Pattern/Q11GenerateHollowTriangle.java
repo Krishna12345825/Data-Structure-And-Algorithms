831f0e9f24672f77d6841f27c8eab2ebab9f9228
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-07 00:10:37 +0530

    Pattern 11

---
+++ src/TapDSA/Pattern/Q11GenerateHollowTriangle.java
@@ -0,0 +1,28 @@
+package TapDSA.Pattern;
+
+import java.util.Scanner;
+
+public class Q11GenerateHollowTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = scanner.nextInt();
+        pattern11(n);
+    }
+
+    public static void pattern11(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (i==n ||  j==1 || j==i) {
+                    System.out.print("*"+" ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
