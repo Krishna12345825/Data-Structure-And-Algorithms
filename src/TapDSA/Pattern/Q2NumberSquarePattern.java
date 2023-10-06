27fd1673332869631bf020e8ef6d183fc3a2add9
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-06 22:13:44 +0530

    Pattern 2

---
+++ src/TapDSA/Pattern/Q2NumberSquarePattern.java
@@ -0,0 +1,20 @@
+package TapDSA.Pattern;
+
+import java.util.Scanner;
+
+public class Q2NumberSquarePattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int n = scanner.nextInt();
+        pattern2(n);
+    }
+    public static void pattern2(int n){
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <=n; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
