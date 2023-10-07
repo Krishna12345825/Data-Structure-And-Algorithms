b8e64ec463de88347a1bbcf1b937cd6b4f0eae23
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-07 12:06:41 +0530

    prime number

--- src/TapDSA/Mathematics/Q4PrimeNUmbers.java
+++ src/TapDSA/Mathematics/Q4PrimeNUmbers.java
@@ -4,7 +4,8 @@ public class Q4PrimeNUmbers {
     public static void main(String[] args) {
         int n= 13;
 //        System.out.println(isPrime1(n));
-        System.out.println(isPrime2(n));
+//        System.out.println(isPrime2(n));
+        System.out.println(isPrime3(n));
     }
 
     public static boolean isPrime1(int n){
@@ -19,6 +20,25 @@ public class Q4PrimeNUmbers {
     public static boolean isPrime2(int n){
         for (int i = 2; i<=Math.sqrt(n); i++) {
             if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static boolean isPrime3(int n){
+        if (n==1){
+            return false;
+        }
+        if (n==2 || n==3){
+            return true;
+        }
+        if (n%2==0 || n%3==0){
+            return false;
+        }
+        for (int i = 5; i*i<=n; i=i+6) {
+            if (n%i==0 || n%(i+2)==0){
                 return false;
             }
         }
