863bdfc7a8da0f268c9cf3055102fa3b02deb2cc
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 20:41:57 +0530

    Write a program to print all the numbers which are multiples of either 2, 5 or 7 till N.

---
+++ src/TapDSA/Programming/Loops/Q9MultiplesOfThreeFiveSeven.java
@@ -0,0 +1,24 @@
+package TapDSA.Programming.Loops;
+
+import java.util.Scanner;
+/*
+Write a program to print the first N multiples of either 3, 5 or 7.
+*/
+
+
+
+public class Q9MultiplesOfThreeFiveSeven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0; // To keep track of the number of multiples found
+        int num = 1; // Starting number
+        while (count < n) {
+            if (num % 3 == 0 || num % 5 == 0 || num % 7 == 0) {
+                System.out.print(num + " ");
+                count++;
+            }
+            num++;
+        }
+    }
+}
