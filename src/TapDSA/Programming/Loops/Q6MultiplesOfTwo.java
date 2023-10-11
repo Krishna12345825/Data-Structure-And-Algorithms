2e6fc6a440ac702f84b40294d0d6188aea36fc8d
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 20:21:10 +0530

    Write a program to print all the multiples of 2 till N.

---
+++ src/TapDSA/Programming/Loops/Q6MultiplesOfTwo.java
@@ -0,0 +1,18 @@
+package TapDSA.Programming.Loops;
+
+import java.util.Scanner;
+
+/*
+Write a program to print all the multiples of 2 till N.
+*/
+
+
+public class Q6MultiplesOfTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=2; i<=n; i=i+2){
+            System.out.print(i+" ");
+        }
+    }
+}
