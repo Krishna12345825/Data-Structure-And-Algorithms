f3582805197c9272250ab3d54e8736d3dcae4c7c
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 21:37:16 +0530

    Write a program to count factors of a given number.

---
+++ src/TapDSA/Programming/Loops/Q13CountFactors.java
@@ -0,0 +1,20 @@
+package TapDSA.Programming.Loops;
+
+import java.util.Scanner;
+
+/*
+Write a program to count factors of a given number.
+*/
+public class Q13CountFactors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
