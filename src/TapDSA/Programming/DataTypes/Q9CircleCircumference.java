dee496b4b9d9e3a6ea4f4a9638985c1ee6cb4495
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 15:45:44 +0530

    add 3 number

---
+++ src/TapDSA/Programming/DataTypes/Q9CircleCircumference.java
@@ -0,0 +1,16 @@
+package TapDSA.Programming.DataTypes;
+
+import java.util.Scanner;
+
+/*
+Print the circumference of the circle. The circumference should have four decimal places only.
+*/
+
+public class Q9CircleCircumference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+        double  circumference = 2 * 3.142 * radius;
+        System.out.printf("%.4f\n", circumference);
+    }
+}
