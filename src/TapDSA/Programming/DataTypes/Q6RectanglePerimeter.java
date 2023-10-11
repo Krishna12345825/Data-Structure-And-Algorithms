455d7b62b27006c756f706c62dac9baec0005f82
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 15:25:31 +0530

    add 3 number

---
+++ src/TapDSA/Programming/DataTypes/Q6RectanglePerimeter.java
@@ -0,0 +1,16 @@
+package TapDSA.Programming.DataTypes;
+
+import java.util.Scanner;
+
+// Write a program to find the perimeter of the rectangle. Formula: perimeter = 2 * (length + width)
+
+public class Q6RectanglePerimeter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int length = scanner.nextInt();
+        int width = scanner.nextInt();
+        int perimeter = 2 * (length + width);
+        System.out.println(perimeter);
+    }
+}
