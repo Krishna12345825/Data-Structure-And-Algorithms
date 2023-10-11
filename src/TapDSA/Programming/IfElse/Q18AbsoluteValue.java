f48689b453f9f9877e542912396d0a2343b2b157
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 18:24:34 +0530

    add 3 number

---
+++ src/TapDSA/Programming/IfElse/Q18AbsoluteValue.java
@@ -0,0 +1,17 @@
+package TapDSA.Programming.IfElse;
+
+import java.util.Scanner;
+
+/*
+Write a program to find the absolute value of a given integer.
+*/
+
+
+public class Q18AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int absoluteValue = Math.abs(n);
+        System.out.println(absoluteValue);
+    }
+}
