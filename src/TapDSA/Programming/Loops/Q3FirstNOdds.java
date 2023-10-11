ff820c9f46d3d9e2b3c77dc8f2825a859d3c3f55
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 18:56:53 +0530

    Write a program to print the first N odd natural numbers.

---
+++ src/TapDSA/Programming/Loops/Q3FirstNOdds.java
@@ -0,0 +1,17 @@
+package TapDSA.Programming.Loops;
+
+import java.util.Scanner;
+/*
+Write a program to print the first N odd natural numbers.
+*/
+
+
+public class Q3FirstNOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=1; i<=n; i++){
+            System.out.print((2 * i)-1+" ");
+        }
+    }
+}
