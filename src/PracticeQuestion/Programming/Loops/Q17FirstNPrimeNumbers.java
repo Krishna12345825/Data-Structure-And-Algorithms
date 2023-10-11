48928a86660850264077c0e4a644b2926b0a9ed5
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 23:02:23 +0530

    Print the first 'n' prime numbers.

---
+++ src/PracticeQuestion/Programming/Loops/Q17FirstNPrimeNumbers.java
@@ -0,0 +1,41 @@
+package PracticeQuestion.Programming.Loops;
+
+import java.util.Scanner;
+
+/*
+Print the first 'n' prime numbers.
+*/
+public class Q17FirstNPrimeNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printFirstNPrimes(n);
+    }
+
+    public static void printFirstNPrimes(int n) {
+        int count = 0;
+        int num = 2;
+
+        while (count < n) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+                count++;
+            }
+            num++;
+        }
+    }
+
+    public static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
