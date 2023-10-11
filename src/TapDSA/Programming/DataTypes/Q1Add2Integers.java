5140be8ef1474353fbbd52e930b731bc66fe0ca3
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 14:51:56 +0530

    add

---
+++ src/TapDSA/Programming/DataTypes/Q1Add2Integers.java
@@ -0,0 +1,15 @@
+package TapDSA.Programming.DataTypes;
+
+
+import java.util.Scanner;
+
+public class Q1Add2Integers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Write your code here
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int sum  = m + n ;
+        System.out.println(sum);
+    }
+}
