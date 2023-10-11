e6db4b62f8a398b63c8a5a729981eaa9baa337d5
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-11 21:29:18 +0530

    Write a program to find factors of a given number.

---
+++ src/TapDSA/Programming/Loops/Q12Factors.java
@@ -0,0 +1,18 @@
+package TapDSA.Programming.Loops;
+
+import java.util.Scanner;
+
+/*
+Write a program to find factors of a given number.
+*/
+public class Q12Factors {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
