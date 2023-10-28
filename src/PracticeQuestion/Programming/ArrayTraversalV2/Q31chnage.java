dc6e9cfe93f456dda0f4ff432619569140036a51
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-10-28 18:14:26 +0530

    Reverse Array Elements

---
+++ src/PracticeQuestion/Programming/ArrayTraversalV2/Q31chnage.java
@@ -0,0 +1,29 @@
+package PracticeQuestion.Programming.ArrayTraversalV2;
+
+/*
+Last Occurrence Index Finder
+        Find the index of the last occurrence of a given character in a string.
+
+        Input Format
+        The first line contains a string.
+
+        The second line contains a single character
+
+        Output Format
+        The program outputs a single line containing the index of the last occurrence of the given character in the input string.
+
+        Sample Inputs & Outputs
+        Sample Input 1
+        Hello World!
+        o
+        Sample Output 1
+        7
+        Sample Input 2
+        Tapacademy
+        a
+        Sample Output 2
+        5
+*/
+
+public class Q31chnage {
+}
---
+++ src/PracticeQuestion/Programming/ArrayTraversalV2/Q32change.java
@@ -0,0 +1,35 @@
+package PracticeQuestion.Programming.ArrayTraversalV2;
+
+/*
+Missing Element
+        Find the missing element in an unsorted array without using sorting.
+
+        Input Format
+        The first line consists of the size of the array, followed by a line containing space-separated integers representing the elements of the array.
+
+        Output Format
+        Output a single integer representing the missing element.
+
+        Sample Inputs & Outputs
+        Sample Input 1
+        6
+        1 4 2 7 5 3
+        Sample Output 1
+        6
+        Sample Input 2
+        10
+        1 2 3 6 8 7 4 5 10 11
+        Sample Output 2
+        9
+        Constraints
+        The length of the array will be positive and will not exceed 1000.
+
+        Each integer in the array will be between 1 and 10^9 (inclusive).
+
+        There will be exactly one element missing from the array.
+*/
+
+
+public class Q32change {
+
+}
