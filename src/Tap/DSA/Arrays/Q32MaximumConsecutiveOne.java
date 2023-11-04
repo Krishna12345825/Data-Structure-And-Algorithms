f1ea7995fece56135531d810bdb8a8a5b44186c4
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-11-05 01:05:34 +0530

    Reverse Array Elements

--- src/Tap/DSA/Arrays/Q32MaximumConsecutiveOne.java
+++ src/Tap/DSA/Arrays/Q32MaximumConsecutiveOne.java
@@ -4,7 +4,7 @@ public class Q32MaximumConsecutiveOne {
     public static void main(String[] args) {
 //        int []nums = new int[]{1,1,0,1,1,1};
         int []nums = new int[]{1,0,1,1,0,1};
-        int result = findMaxConsecutiveOnes(nums);
+        int result = findMaxConsecutiveOnes1(nums);
         System.out.println(result);
     }
 
@@ -21,6 +21,21 @@ public class Q32MaximumConsecutiveOne {
                     break;
             }
             maxCount = Math.max(count, maxCount);
+        }
+        return maxCount;
+    }
+
+
+    static int findMaxConsecutiveOnes1(int[] arr) {
+        int maxCount = 0;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==0){
+                count = 0;
+            }else {
+                count++;
+                maxCount = Math.max(count, maxCount);
+            }
         }
         return maxCount;
     }
