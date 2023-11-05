1a1bedba0ad437b730c5115a8e6e0884b57662b4
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-11-05 11:03:10 +0530

    Reverse Array Elements

---
+++ src/Tap/DSA/Arrays/Q33MaximumSubArray.java
@@ -0,0 +1,23 @@
+package Tap.DSA.Arrays;
+
+public class Q33MaximumSubArray {
+    public static void main(String[] args) {
+        int arr[] = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int result = maximumSubArray(arr);
+        System.out.println(result);
+    }
+
+
+    // naive approach
+    static  int maximumSubArray(int arr[]){
+        int maxSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum = sum + arr[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+}
