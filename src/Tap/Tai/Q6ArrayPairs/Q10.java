147dce032b052eba48ef1c8eec7cec466da96be5
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-11-10 21:17:13 +0530

    Reverse Array Elements

--- src/Tap/Tai/Q6ArrayPairs/Q10.java
+++ src/Tap/Tai/Q6ArrayPairs/Q10.java
@@ -3,22 +3,22 @@ package Tap.Tai.Q6ArrayPairs;
 import java.util.Scanner;
 /*
 
-Odd Pairs
+Counting Pairs
 Sample input :-
-6
-2 4 6 3 8 9
-sample output:-
-3 9
-Sample input :-
-4
-1 2 3 4
+5
+1 2 3 4 5
 
 sample output:-
-1 3
+10
+Sample input :-
+3
+-1 0 1
+sample output:-
+3
 */
 
 
-public class Q9 {
+public class Q10 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -26,17 +26,18 @@ public class Q9 {
         for (int i = 0; i<arr.length; i++) {
             arr[i] = scanner.nextInt();
         }
-        oddPairs(arr);
+        int res =  countPairs(arr);
+        System.out.println(res);
     }
 
 
-    static void oddPairs(int arr[]){
+    static int countPairs(int arr[]){
+        int count  = 0;
         for (int i = 0; i<arr.length; i++) {
             for (int j = i + 1; j<arr.length; j++) {
-                if (arr[i]%2!=0  &&  arr[j]%2!=0){
-                    System.out.println(arr[i] +" "+ arr[j]);
-                }
+                count++;
             }
         }
+        return count;
     }
 }
