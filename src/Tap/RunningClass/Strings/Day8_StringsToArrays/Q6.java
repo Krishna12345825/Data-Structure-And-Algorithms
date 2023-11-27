f948fa9090cfb0964c606f75ff3eb1723e30e9cd
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-11-27 21:02:24 +0530

    Reverse Array Elements

---
+++ src/Tap/RunningClass/Strings/Day8_StringsToArrays/Q6.java
@@ -0,0 +1,60 @@
+package Tap.RunningClass.Strings.Day8_StringsToArrays;
+
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String arr[] = split(s);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+        for (int i = 0, j= arr.length -1; i < j;  i++, j--) {
+            System.out.print(arr[i] +" "+ arr[j]+" ");
+        }
+
+    }
+
+    static String reverseString(String s){
+        String t = "";
+        for (int i = 0; i < s.length(); i++) {
+            t = s.charAt(i) + t;
+        }
+        return t;
+    }
+
+
+    static int countWords(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == ' ' && s.charAt(i + 1) != ' ') {
+                count++;
+            }
+        }
+        return s.charAt(0) == ' ' ? count : count + 1;
+    }
+
+
+    static String[] split(String s) {
+        int wordCount = countWords(s);
+        String[] arr = new String[wordCount];
+        String t = "";
+        int j = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                t = t + s.charAt(i);
+            } else if (t.length() > 0) {
+                arr[j] = t;
+                j++;
+                t = "";
+            }
+        }
+        arr[j] = t;
+        return arr;
+    }
+
+
+}
