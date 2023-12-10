5e437c45079a512728f60d00f367b5cadfc10685
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-10 22:31:55 +0530

    Reverse Array Elements

--- src/Tap/DSA/LinkedList/DoublyLinkedList.java
+++ src/Tap/DSA/LinkedList/DoublyLinkedList.java
@@ -2,8 +2,8 @@ package Tap.DSA.LinkedList;
 
 class Node1 {
      int data;
-     Node next;
-     Node prev;
+     Node1 next;
+     Node1 prev;
 
     Node1(int data) {
         this.data = data;
@@ -16,16 +16,37 @@ class Dll {
     Node1 head;
     Node1 tail;
 
-
+    void add(int data){
+        Node1 temp = new Node1(data);
+        if (head == null){
+            head = temp;
+            tail = temp;
+        }else {
+            tail.next = temp;
+            temp.prev = tail;
+            tail = temp;
+        }
+    }
 
 }
 
 public class DoublyLinkedList {
     public static void main(String[] args) {
 
-        Node1 list = new Node1(10);
-        System.out.println(list.data);
-        System.out.println(list.prev);
-        System.out.println(list.next);
+//        Node1 list = new Node1(10);
+//        System.out.println(list.data);
+//        System.out.println(list.prev);
+//        System.out.println(list.next);
+
+        Dll list = new Dll();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+//        System.out.println(list.head.data);
+//        System.out.println(list.tail.data);
+
+
     }
 }
