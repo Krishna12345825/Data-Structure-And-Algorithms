9fb80e0445c1b9c1a6fac1884c07db9231b0405a
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-11 20:35:17 +0530

    new changes

--- src/Tap/RunningClass/collections/Map/Q5.java
+++ src/Tap/RunningClass/collections/Map/Q5.java
@@ -1,7 +1,23 @@
 package Tap.RunningClass.collections.Map;
 
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Q5 {
+
+    static void removeDuplicates(int arr[]){
+        TreeSet<Integer>  set = new TreeSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        for (int ar : set){
+            System.out.println(ar);
+        }
+    }
+
     public static void main(String[] args) {
+       int arr[] = {10, 2,7,3,5,7,2,3};
+       removeDuplicates(arr);
 
     }
 }
