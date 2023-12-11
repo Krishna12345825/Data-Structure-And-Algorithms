1ea761da607eb7eac8533f85d67bbd48aea1b9e1
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-11 15:49:27 +0530

    new changes

--- src/Tap/RunningClass/collections/Map/Q1.java
+++ src/Tap/RunningClass/collections/Map/Q1.java
@@ -2,18 +2,40 @@ package Tap.RunningClass.collections.Map;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Q1 {
 
     public static void main(String[] args) {
 
-        HashMap map = new HashMap<>();
+//        HashMap map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
         map.put("india", 900);
         map.put("china", 9700);
         map.put("usa", 8900);
         map.put("uk", 8769);
-        map.put(true, false);
-        map.put(3.14, new ArrayList<>());
-        System.out.println(map);
+//        map.put(true, false);
+//        map.put(3.14, new ArrayList<>());
+//        System.out.println(map);
+
+
+
+/*      Set<Map.Entry<String, Integer>>  entrys = map.entrySet();
+      for (Map.Entry<String, Integer> e : entrys){
+//          System.out.println(e);
+          System.out.println(e.getKey() + " : " + e.getValue());
+      }*/
+
+
+        Set<String> keys = map.keySet();
+        for (String key : keys){
+            Integer value = map.get(key);
+            System.out.println(key + " : " + value);
+        }
+
+
+
+
     }
 }
