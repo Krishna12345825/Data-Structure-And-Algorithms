851725bbda40ad79e530df7c423889b2ce75bbc8
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-11 21:03:51 +0530

    new changes

--- src/Tap/RunningClass/collections/Map/Q7.java
+++ src/Tap/RunningClass/collections/Map/Q7.java
@@ -1,4 +1,41 @@
 package Tap.RunningClass.collections.Map;
 
+import java.util.*;
+
+/*
+input:-
+        mississippi
+*/
+
+
 public class Q7 {
+
+    static String rearrange(String s){
+//        HashMap<Character, Integer> map = new HashMap<>();
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)){
+                Integer value = map.get(ch);
+                map.put(ch, value + 1);
+            }else {
+                map.put(ch, 1);
+            }
+        }
+        Set<Map.Entry<Character, Integer>> entries = map.entrySet();
+        String t = "";
+        for (Map.Entry<Character, Integer> e : entries){
+            t = t + e.getKey() + e.getValue();
+        }
+        return t;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String result = rearrange(s);
+        System.out.println(result);
+    }
 }
+
