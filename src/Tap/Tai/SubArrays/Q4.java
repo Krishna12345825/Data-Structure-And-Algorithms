44fd51a4fd0b487baf4cf046c7978487187fb148
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-13 12:01:20 +0530

    new changes

--- src/Tap/Tai/SubArrays/Q4.java
+++ src/Tap/Tai/SubArrays/Q4.java
@@ -1,56 +1,55 @@
 package Tap.Tai.SubArrays;
 
-
-/*
-Longest SubArray with Sum Equals to K
-
-input1:-
-7
-8
-3 4 7 2 -3 1 4 2
-output1:-
-7 2 -3 1
-
-input2:-
-10
-6
-1 2 3 4 5 6
-
-output2:-
-1 2 3 4
-*/
-
-
 import java.util.Scanner;
 
-public class Q3 {
+public class Q4 {
+
+    /*
+
+Smallest SubArray with Sum Equals to K
+input1:
+5
+6
+2 3 1 4 2 5
+output1:-
+5
+
+input2:-
+15
+5
+10 2 4 8 7
+output2:-
+8 7
 
 
-    static void longestSubArrayWithSumK(int k, int arr[]){
-        int maxLength = 0;
+
+ */
+
+
+    static void smallestSubArraySumK(int k, int arr[]){
         int startingIndex = -1;
         int endingIndex = -1;
+        int minLength =Integer.MAX_VALUE;
         for (int size = 0; size < arr.length; size++) {
             for (int i = 0; i < arr.length - size; i++) {
                 int sum = 0;
                 for (int j = i; j <= i + size; j++) {
                     sum = sum + arr[j];
                 }
-                if (sum == k && size > maxLength){
-                    maxLength = size;
+                if (sum == k && size < minLength){
+                    minLength = size + 1;
                     startingIndex = i;
                     endingIndex = i + size;
                 }
             }
         }
-
-        if (maxLength > 0){
-            for (int i = startingIndex; i <= endingIndex; i++) {
+        if (minLength  != Integer.MAX_VALUE){
+            for (int i = startingIndex; i <= endingIndex ; i++) {
                 System.out.print(arr[i]+" ");
             }
             System.out.println();
         }else {
-            System.out.println("No SubArray found");
+            System.out.println("No found SubArray");
         }
     }
 
@@ -62,6 +61,6 @@ public class Q3 {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scanner.nextInt();
         }
-        longestSubArrayWithSumK(k, arr);
+        smallestSubArraySumK(k, arr);
     }
 }
