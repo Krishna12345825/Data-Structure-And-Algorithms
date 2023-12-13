c64387b628a1a54e4440cedd56c12048f4058c39
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-13 11:42:06 +0530

    new changes

---
+++ src/Tap/Tai/SubArrays/Q3.java
@@ -0,0 +1,65 @@
+package Tap.Tai.SubArrays;
+
+
+/*
+Longest SubArray with Sum Equals to K
+
+input1:-
+7
+8
+3 4 7 2 -3 1 4 2
+output1:-
+7 2 -3 1
+
+input2:-
+10
+6
+1 2 3 4 5 6
+
+output2:-
+1 2 3 4
+*/
+
+
+import java.util.Scanner;
+
+public class Q3 {
+
+    /*Longest SubArray with Sum Equals to K*/
+    static void longestSubArrayWithSumK(int k, int arr[]){
+        int maxLength = 0;
+        int startingIndex = -1;
+        int endingIndex = -1;
+        for (int size = 0; size < arr.length; size++) {
+            for (int i = 0; i < arr.length - size; i++) {
+                int sum = 0;
+                for (int j = i; j <= i + size; j++) {
+                    sum = sum + arr[j];
+                }
+                if (sum == k && size > maxLength){
+                    maxLength = size;
+                    startingIndex = i;
+                    endingIndex = i + size;
+                }
+            }
+        }
+
+        if (maxLength > 0){
+            for (int i = startingIndex; i <= endingIndex; i++) {
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k = scanner.nextInt();
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        longestSubArrayWithSumK(k, arr);
+    }
+}
