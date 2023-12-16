430cb02d17961963e4c67d66967ca74b8a48189b
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-16 23:14:38 +0530

    new changes

--- src/Demo.java
+++ src/Demo.java
@@ -2,36 +2,48 @@ import java.util.*;
 
 class Demo {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int arr[] = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = scanner.nextInt();
+        Scanner sc  = new Scanner(System.in);
+        String s = sc.nextLine();
+        String arr[] = split(s);
+        for (int i = 0, j = arr.length-1; i < j; i++, j--) {
+
+            System.out.print(reverse(arr[j])+ arr[i]+" ");
         }
-        sumPrime(arr);
+
+    }
+    static String reverse(String s){
+        String t = "";
+        for (int i = 0; i<s.length(); i++){
+            t = s.charAt(i) + t;
+        }
+        return t;
     }
 
-    static  void sumPrime(int arr[]){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                int sum = arr[i] + arr[j];
-                if (isPrime(sum)){
-                    System.out.println(arr[i] +" " + arr[j]);
-                }
+    static String[] split(String s){
+        int n = countWord(s);
+        String t = "";
+        int j = 0;
+        String arr[] = new String[n];
+        for (int i = 0; i<s.length(); i++){
+            if (s.charAt(i) != ' '){
+                t = t + s.charAt(i);
+            } else if (t.length() > 0) {
+                arr[j] = t;
+                j++;
+                t = "";
             }
         }
+        arr[j] = t;
+        return arr;
     }
-
-    static boolean isPrime(int n){
-        if (n <= 1){
-            return false;
-        }
-        for (int i = 2; i *i<= n; i++) {
-            if (n%i==0){
-                return false;
+    static int countWord(String s){
+        int count = 0;
+        for(int i = 0; i<s.length()-1; i++){
+            if(s.charAt(i) == ' ' && s.charAt(i+1) != ' '){
+                count++;
             }
         }
-        return true;
+        return s.charAt(0)== ' ' ? count : count+1;
     }
 }
 
