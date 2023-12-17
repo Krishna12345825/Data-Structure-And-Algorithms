41e837cf02c96ff05b5672146a6adb808a502e67
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-17 13:13:59 +0530

    new changes

--- src/Tap/RunningClass/Arrays/Day20_ConsecutiveSubArrays/Q5.java
+++ src/Tap/RunningClass/Arrays/Day20_ConsecutiveSubArrays/Q5.java
@@ -12,8 +12,43 @@ output:-
 
 */
 
+import java.util.Scanner;
+
 public class Q5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        largestConsecutiveSubArray(arr);
+    }
+
+    static void largestConsecutiveSubArray(int arr[]){
+        int count = 1;
+        int max = 0;
+        int startingIndex = 0;
+        int endingIndex = 0;
+        for (int i = 0; i<arr.length-1; i++){
+            if (arr[i+1] - arr[i] == 1){
+                count++;
+            }else {
+                if (count > max){
+                    max = count;
+                    endingIndex = i;
+                }
+                count = 1;
+            }
+        }
+        if (count > max){
+            max = count;
+            endingIndex = arr.length-1;
+        }
+        startingIndex = endingIndex - max + 1;
+        for (int i = startingIndex; i <= endingIndex; i++){
+            System.out.print(arr[i] + " ");
+        }
 
     }
 
