51910aa800e408e259d0b1b20a7a7de5b5682c25
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-17 15:01:27 +0530

    new changes

--- src/Tap/RunningClass/Arrays/Day19_RearrangeTheArray/Q1.java
+++ src/Tap/RunningClass/Arrays/Day19_RearrangeTheArray/Q1.java
@@ -1,4 +1,51 @@
 package Tap.RunningClass.Arrays.Day19_RearrangeTheArray;
+/*
+
+Given an Array of positive Integer values and -1. Write a program to put all the -1 to the starting of the array
+
+intput:-
+10
+5 -1 3 -1 4 5 3 -1 8 2
+
+output:-
+-1 -1 -1 5 3 4 5 3 8 2
+
+*/
+
+
+import java.util.Scanner;
 
 public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for (int i= 0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        int res[] = rearrange(arr);
+        for (int i = 0; i<res.length; i++){
+            System.out.print(res[i]+" ");
+        }
+    }
+
+    static int[] rearrange(int arr[]){
+        int i = arr.length-1;
+        int j = arr.length-1;
+
+        while (i >=0 ){
+            if (arr[i] == -1){
+                i--;
+            }else {
+                arr[j] = arr[i];
+                i--;
+                j--;
+            }
+        }
+        while (j >= 0){
+            arr[j] = -1;
+            j--;
+        }
+        return arr;
+    }
 }
---
+++ src/Tap/RunningClass/Arrays/Day19_RearrangeTheArray/Q2.java
@@ -0,0 +1,53 @@
+package Tap.RunningClass.Arrays.Day19_RearrangeTheArray;
+
+import java.util.Scanner;
+
+
+
+/*
+
+Given an Array of positive Integer values and 0. Write a program to put all the 0 to the ending of the array
+
+intput:-
+10
+3 7 0 4 0 6 0 8 0 3
+
+output:-
+3 7 4 6 8 3 0 0 0 0
+
+*/
+
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for (int i =0; i<arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        int res[] = rearrange(arr);
+        for (int i=0; i<res.length; i++){
+            System.out.print(res[i] + " ");
+        }
+    }
+
+    static int[] rearrange(int arr[]){
+        int i = 0;
+        int j = 0;
+        while (i < arr.length){
+            if (arr[i] == 0){
+                i++;
+            }else {
+                arr[j] = arr[i];
+                i++;
+                j++;
+            }
+        }
+        while (j < arr.length){
+            arr[j] = 0;
+            j++;
+        }
+        return arr;
+    }
+}
