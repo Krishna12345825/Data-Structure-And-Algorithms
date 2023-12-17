dedd2f7d43bc68088d1219f6a0bb7c0e3cf3ea1a
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-17 12:44:16 +0530

    new changes

---
+++ src/Tap/RunningClass/Arrays/Day20_ConsecutiveSubArrays/Q5.java
@@ -0,0 +1,21 @@
+package Tap.RunningClass.Arrays.Day20_ConsecutiveSubArrays;
+
+
+/*
+Given an array of size n, Print the Largest subArray that is consecutive
+input:-
+10
+5 12 13 14 9 2 3 4 5 6
+
+output:-
+2 3 4 5 6
+
+*/
+
+public class Q5 {
+    public static void main(String[] args) {
+
+    }
+
+
+}
