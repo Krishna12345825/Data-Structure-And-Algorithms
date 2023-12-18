1a53c73e4b8c2cc8a88112011eded8d3b07a9d74
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-18 15:00:37 +0530

    new changes

---
+++ src/Tap/RunningClass/Arrays/Day13_ProgramsOnSortedArray/Q2.java
@@ -0,0 +1,37 @@
+package Tap.RunningClass.Arrays.Day13_ProgramsOnSortedArray;
+
+/*
+   Given a sorted array of Integer Values print all the elements of the array without repeating.
+
+   input:-
+   8
+   2 2 2 3 4 4 4 23
+
+   output:-
+  2 3 4 23
+
+* */
+
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        withoutRepeatingElements(arr);
+    }
+
+    static void withoutRepeatingElements(int arr[]){
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i+1]){
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.print(arr[arr.length-1]);
+    }
+
+}
