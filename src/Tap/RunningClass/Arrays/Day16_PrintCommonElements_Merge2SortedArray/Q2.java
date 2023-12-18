55011c67db7eda545c4643ec4e381b3c75439c39
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-18 12:41:36 +0530

    new changes

--- src/Tap/RunningClass/Arrays/Day16_PrintCommonElements_Merge2SortedArray/Q2.java
+++ src/Tap/RunningClass/Arrays/Day16_PrintCommonElements_Merge2SortedArray/Q2.java
@@ -3,11 +3,51 @@ package Tap.RunningClass.Arrays.Day16_PrintCommonElements_Merge2SortedArray;
 
 /*
 Given 2 sorted array print the element which are repeated.
+input:-
+arr1:- 3 5 6 8 9
+arr2:- 2 3 4 6 7 8 8
+
+output:-
+3 6 8
 
 
 */
 
 
+import java.util.Scanner;
+
 public class Q2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr1[] = new int[n];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int arr2[] = new int[m];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = scanner.nextInt();
+        }
+        repeatedElements(arr1, arr2);
+
+    }
+
+    static void repeatedElements(int arr1[], int arr2[]){
+        int i = 0;
+        int j = 0;
+        while (i < arr1.length && j < arr2.length){
+            if (arr1[i] == arr2[j]){
+                System.out.print(arr1[i] + " ");
+                i++;
+                j++;
+            } else if (arr2[j] < arr1[i]) {
+                j++;
+            }else {
+                i++;
+            }
+        }
+    }
+
 
 }
