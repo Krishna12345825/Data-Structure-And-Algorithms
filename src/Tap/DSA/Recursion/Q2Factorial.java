37893ad3d684b071d304f942c926aa5cd326a21c
KRISHNA KUMAR <krishnabhai117@gmail.com>
2023-12-26 17:22:08 +0530

    new changes

--- src/Tap/DSA/Recursion/Q2Factorial.java
+++ src/Tap/DSA/Recursion/Q2Factorial.java
@@ -1,11 +1,12 @@
 package Tap.DSA.Recursion;
 
-public class Factorial {
+public class Q2Factorial {
     public static void main(String[] args) {
         System.out.println(factorial(5));
     }
     public static int factorial(int n){
-        if(n<=1)
+//        if(n<=1)
+        if (n == 0 || n == 1)
             return 1;
         return n*factorial(n-1);
     }
