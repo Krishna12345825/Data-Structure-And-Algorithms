e177e391daeb49e4f1760bbf52fe757c4c5a7d09
KRISHNA KUMAR <krishnabhai117@gmail.com>
2024-02-06 22:35:34 +0530

    new changes

---
+++ src/Tap/Tai/Q10_StringTraversal_Level1/Q5.java
@@ -0,0 +1,28 @@
+package Tap.Tai.Q10_StringTraversal_Level1;
+/*
+
+Odd Index Character Printer
+input :
+Testing
+output:
+e t n
+
+*/
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        oddIndexing(s);
+    }
+
+    static void oddIndexing(String s){
+        for (int i = 0; i < s.length(); i++) {
+            if (i % 2 != 0){
+                System.out.println(s.charAt(i));
+            }
+        }
+    }
+}
