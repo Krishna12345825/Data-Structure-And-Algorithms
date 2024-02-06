a441ee8e613a3a0ec81ffd59ca7dd78aa8bb22db
KRISHNA KUMAR <krishnabhai117@gmail.com>
2024-02-06 21:58:13 +0530

    new changes

---
+++ src/Tap/Tai/Q10_StringTraversal_Level1/Q1.java
@@ -0,0 +1,38 @@
+package Tap.Tai.Q10_StringTraversal_Level1;
+/*
+
+print all characters
+
+input :   KrishnaKumar
+output:
+k
+r
+i
+s
+h
+n
+a
+K
+u
+m
+a
+r
+*/
+
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        printAllCharector(s);
+    }
+
+    static void printAllCharector(String s){
+        for (int i = 0; i<s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+
+}
