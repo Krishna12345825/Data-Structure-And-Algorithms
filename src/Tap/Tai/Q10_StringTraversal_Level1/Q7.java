3612db9a3ddca465e4268c5b50de1347933463be
KRISHNA KUMAR <krishnabhai117@gmail.com>
2024-02-06 22:48:29 +0530

    new changes

---
+++ src/Tap/Tai/Q10_StringTraversal_Level1/Q7.java
@@ -0,0 +1,30 @@
+package Tap.Tai.Q10_StringTraversal_Level1;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        String result = reverseString(s);
+        System.out.println(result);
+    }
+
+   /*
+   static String reverseString(String s){
+        String t = "";
+        for (int i = 0; i<s.length(); i++){
+            t =  s.charAt(i) + t;
+        }
+        return t;
+    }
+    */
+
+    static String reverseString(String s){
+        String t = "";
+        for (int i = s.length()-1; i >=0; i--) {
+            t = t + s.charAt(i);
+        }
+        return t;
+    }
+}
