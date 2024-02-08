a5e2d741f35cea005d01371b6442aab7a211f067
KRISHNA KUMAR <krishnabhai117@gmail.com>
2024-02-08 08:51:25 +0530

    new changes

---
+++ src/Tap/Tai/Q11_StringTraversal_Level2/Q6.java
@@ -0,0 +1,36 @@
+package Tap.Tai.Q11_StringTraversal_Level2;
+
+import java.util.Scanner;
+
+/*
+
+Remove Lowercase Characters
+
+input:
+        PpRrOoGgRrAaMmMmIiNnGg
+
+        output:
+
+        PROGRAMMING
+*/
+
+
+public class Q6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String result = removeLowercaseCharacters(s);
+        System.out.println(result);
+    }
+
+    static String removeLowercaseCharacters(String s){
+        String t = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch >= 'A' && ch <= 'Z'){
+                t = t + ch;
+            }
+        }
+        return t;
+    }
+}
---
+++ src/Tap/Tai/Q11_StringTraversal_Level2/Q7.java
@@ -0,0 +1,37 @@
+package Tap.Tai.Q11_StringTraversal_Level2;
+
+import java.util.Scanner;
+
+/*
+
+Remove Vowel
+
+input1:
+        coding
+
+        output:
+
+        cdng
+
+*/
+
+public class Q7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String result = removeVowel(s);
+        System.out.println(result);
+    }
+
+    static String removeVowel(String s){
+        String t = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(!(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'
+                    || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')){
+                t = t + ch;
+            }
+        }
+        return t;
+    }
+}
